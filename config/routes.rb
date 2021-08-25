0a48c7a7f6a12e1d361786b80d948fee54ef0cbc
hayden <1haydengragnano@gmail.com>
2021-08-25 10:09:00 -0700

    adds a route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ get "/one_product", controller: "products", action: "one_product"
+ get "/all_products", controller: "products", action: "all_products"
 end
